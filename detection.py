52c532ff8353dcbf82cf7fa491a3c88fd48436b8
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-06-21 18:49:39 +0300

    Add class CodeDetection;

---
+++ detection.py
@@ -0,0 +1,68 @@
+import torch
+import cv2
+import numpy as np
+import time
+from pylibdmtx.pylibdmtx import decode
+
+
+class CodeDetection():
+    def __init__(self):
+        self.model = self.load_model()
+        self.classes = self.model.names
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print("\n\nDevice Used:", self.device)
+
+    def load_model(self):
+        model = torch.hub.load('ultralytics/yolov5',
+                               'custom',
+                               path='yolov5/runs/train/exp/weights/datamatrix-best.pt',
+                               )
+        return model
+
+    def score_frame(self, frame):
+        self.model.to(self.device)
+        frame = [frame]
+        results = self.model(frame)
+        labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+        return labels, cord
+
+    def plot_boxes(self, results, frame):
+        labels, cord = results
+        n = len(labels)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            if row[4] >= 0.2:
+                x1, y1, x2, y2 = int(row[0] * x_shape), int(row[1] * y_shape), int(row[2] * x_shape), int(
+                    row[3] * y_shape)
+                bgr = (0, 255, 0)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+        return frame
+
+    def decode_dmx(self, frame):
+        pass
+
+    def __call__(self):
+        cap = cv2.VideoCapture(0)
+
+        while cap.isOpened():
+            start_time = time.perf_counter()
+            ret, frame = cap.read()
+            if not ret:
+                break
+            results = self.score_frame(frame)
+            frame = self.plot_boxes(results, frame)
+            end_time = time.perf_counter()
+            fps = 1 / np.round(end_time - start_time, 3)
+            cv2.putText(frame, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
+            cv2.imshow("img", frame)
+            key = cv2.waitKey(1)
+            if key == ord("p"):
+                cv2.waitKey(3000)
+                print(decode(frame, timeout=2800))
+            elif key & 0xFF == ord('q'):
+                break
+
+
+detection = CodeDetection()
+detection()
